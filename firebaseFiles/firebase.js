dd5d9143ea780cec7694919ec9bf30e3f55764b1
Tanatos <dcb97.kts@gmail.com>
2025-03-07 10:38:18 -0600

    heroku

--- firebaseFiles/firebase.js
+++ firebaseFiles/firebase.js
@@ -1,14 +1,23 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
-// Inicializar Firebase con el archivo de credenciales
-const serviceAccount = require("../config/CondogminioCredentials.json");
+// Cargar las credenciales de Firebase desde las variables de entorno
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Heroku almacena los saltos de línea con '\n' en lugar de realmente ser saltos de línea
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
+// Inicializar Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://condogminio.firebaseio.com", // Asegúrate de usar la URL correcta
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
 
-// Acceder a Firestore
-const db = admin.firestore();
-
-module.exports = db;
+module.exports = admin.firestore(); // O el servicio que estés usando
