ac13ade53de64edd7df737eb3fcc2ae5346b5e05
Tanatos <dcb97.kts@gmail.com>
2025-03-18 14:46:41 -0600

    Routes e index update v.0.0.1

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/routes');  // Rutas de usuarios
 const petRoutes = require('./routes/routes');    // Rutas de mascotas
 const condominioRoutes = require('./routes/routes');  // Rutas de condominios
 const adminRoutes = require('./routes/routes');  // Rutas de administradores
+const pathRoutes = require('./routes/routes');  // Rutas de recorridos
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +18,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/pets', petRoutes);
 app.use('/api/condominios', condominioRoutes);
 app.use('/api/admins', adminRoutes);
+app.use('/api/paths', pathRoutes);
 
 app.get('/', (req, res) => {
   res.send('¡La API está funcionando correctamente!');
